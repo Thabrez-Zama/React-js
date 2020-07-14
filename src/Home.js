5b94503511db254d8beb08249078e072be33e199
Thabrez Zama <thabrez918@gmail.com>
2020-07-15 00:25:13 +0530

    First commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Home=()=>{
+    return(
+      <h1>Its Home Component</h1>
+    );
+  }
+
+  export default Home;
---
+++ src/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from'react';
+
+let Greeting=(props)=> {
+    console.log(props);
+
+    return(
+        <div>
+        <h1>this is {props.name} this is my mail {props.mail}</h1>
+        {props.children}
+        </div>
+    );
+}
+
+export default Greeting;
