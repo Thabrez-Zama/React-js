1a1551c0eaec2af84b2651af2af3d68764ca2459
Thabrez Zama <thabrez918@gmail.com>
2020-07-15 00:29:11 +0530

    Updates

---
+++ precache-manifest.5378bcbfd3cf2bb9468e1e86cf24f711.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca341b03faf9b5f910aa3ecf252ac51",
+    "url": "/React-js/index.html"
+  },
+  {
+    "revision": "4ba5d41a8b1892a1a112",
+    "url": "/React-js/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "472770a62b471d9f2bb5",
+    "url": "/React-js/static/css/main.3458d3fd.chunk.css"
+  },
+  {
+    "revision": "4ba5d41a8b1892a1a112",
+    "url": "/React-js/static/js/2.529ba1b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-js/static/js/2.529ba1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472770a62b471d9f2bb5",
+    "url": "/React-js/static/js/main.ebc086dd.chunk.js"
+  },
+  {
+    "revision": "bf64bb400cd7d97fb603",
+    "url": "/React-js/static/js/runtime-main.d8623275.js"
+  },
+  {
+    "revision": "61a8b2cee865eb822352bed7043ad48f",
+    "url": "/React-js/static/media/single_user.61a8b2ce.png"
+  }
+]);
